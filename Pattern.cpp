e511ef164cccb404c2b55a4a0db38ca249e222d5
Richard <richard@codemoot.com>
2015-10-05 22:17:45 -0700

    first commit

---
+++ Pattern.cpp
@@ -0,0 +1,16 @@
+#include "Pattern.hpp"
+
+void Pattern::Glider( int x , int y )
+{
+	
+};
+
+void Pattern::Blinker( int x , int y )
+{
+	
+};
+
+void Pattern::Gun( int x , int y )
+{
+	
+};
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "Board.hpp"
+
+int main()
+{
+	Board testing(0);
+	testing.displayBoard();
+	Board testing2(1);
+	testing2.displayBoard();
+	system("pause");
+}
