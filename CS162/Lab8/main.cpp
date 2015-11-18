fd116ec2a3918332e86fa9483a20bce4e900e300
richard <richard@codemoot.com>
2015-11-17 22:13:32 -0800

    have doubly linked queue working correctly

--- CS162/Lab8/main.cpp
+++ CS162/Lab8/main.cpp
@@ -1,5 +1,19 @@
 #include "queue.hpp"
+#include <iostream>
 
 int main(){
-  
+  Queue test;
+  test.addBack(5);
+  std::cout << test.getFront() << std::endl;
+  test.addBack(7);
+  test.removeFront();
+  std::cout << test.getFront() << std::endl;
+
+  for(int i = 20; i > 0; i--){
+    test.addBack(i);
+  }
+  for(int j = 21; j > 0; j--){
+    test.removeFront();
+    std::cout << test.getFront() << std::endl;
+  }
 }
--- CS162/Lab8/queue.cpp
+++ CS162/Lab8/queue.cpp
@@ -1,32 +1,38 @@
 #include "queue.hpp"
 
 Queue::Queue(){
-  front = back;
-  back = front;
+  Queuenode* newNode = new Queuenode;
+  newNode->data = -1;
+  newNode->next = newNode;
+  newNode->prev = newNode;
+  back = newNode;
+  front = newNode;
 }
 
 void Queue::addBack(int incData){
   Queuenode* newNode = new Queuenode;
   newNode->data = incData;
-  if(front == back){
+  if(back->data == -1){
     newNode->next = newNode;
     newNode->prev = newNode;
     front = newNode;
     back = newNode;
     return;
   }
-  newNode->next = back->next;
+  newNode->next = front;
+  back->next = newNode;
   newNode->prev = back;
   back = newNode;
 }
 
 int Queue::getFront(){
-  if(front == back){
+  if(back->data == -1){
     return -1;
   }
   return front->data;
 }
 
 void Queue::removeFront(){
-
+  front->data = -1;
+  front = front->next;
 }
