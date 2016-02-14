3dc439653d523bb5c3765b430c25332a6d84b187
Richard <richard@codemoot.com>
2016-02-14 12:46:09 -0800

    fixed openweathermap API calls

--- CS290/AJAX/script.js
+++ CS290/AJAX/script.js
@@ -1,10 +1,16 @@
 function weatherReq(){
   var req = new XMLHttpRequest(),
-      appID = "APPID=5dcaafafdf3ebfd527a59c9e15f6947e",
+      appID = "&APPID=5dcaafafdf3ebfd527a59c9e15f6947e",
       zipCode = document.getElementById('zip').value,
-      url = "http://api.openweathermap.org/data/2.5/forecast/city?zip=" + zipCode + "&" + appID;
+      units = "&units=imperial",
+      url = "http://api.openweathermap.org/data/2.5/forecast/city?zip=" + zipCode + units +  appID,
+      resp;
+  document.getElementById('city').innerText = "";
+  document.getElementById('city').style.color = 'black';
+  document.getElementById('temperature').innerText = "";
   if(zipCode.length != 5){
-    document.getElementById('current-weather').appendChild(document.createTextNode('Invalid Zip Code'));
+    document.getElementById('city').appendChild(document.createTextNode('Invalid Zip Code'));
+    document.getElementById('city').style.color = 'red';
   }
   else{
     req.open("GET", url, true);
@@ -12,8 +18,15 @@ function weatherReq(){
   }
   req.onreadystatechange = function(){
     if(req.readyState == 4 && req.status == 200){
-      console.log(JSON.parse(req.response));
-      document.getElementById('current-weather').appendChild(document.createTextNode(req.response))
+      resp = JSON.parse(req.response);
+      console.log(resp);
+      document.getElementById('city').innerText = resp.city.name;
+      document.getElementById('temperature').innerText = "Current temprature: " + resp.list[0].main.temp + "\xB0 F";
+      document.getElementById('clouds').innerText = "Description: " + resp.list[0].weather[0].description;
     }
   }
+}
+
+function httpPost(){
+
 }
