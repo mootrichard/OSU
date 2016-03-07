fd9ae4da835fc4e6019b758ceaf03c8996af84d4
richard <richard@codemoot.com>
2016-03-06 19:04:29 -0800

    updating the tinkering that I was doing

--- CS290/NodeJS/server.js
+++ CS290/NodeJS/server.js
@@ -43,6 +43,53 @@ app.get('/count', function(req, res) {
   res.render('count', context);
 })
 
+app.get('/session',function(req,res,next){
+  var context = {};
+  //If there is no session, go to the main page.
+  if(!req.session.name){
+    res.render('newSession', context);
+    return;
+  }
+  context.name = req.session.name;
+  context.toDoCount = req.session.toDo.length || 0;
+  context.toDo = req.session.toDo || [];
+  console.log(context.toDo);
+  res.render('toDo',context);
+});
+
+app.post('/session',function(req,res){
+  var context = {};
+
+  if(req.body['New List']){
+    req.session.name = req.body.name;
+    req.session.toDo = [];
+    req.session.curId = 0;
+  }
+
+  //If there is no session, go to the main page.
+  if(!req.session.name){
+    res.render('newSession', context);
+    return;
+  }
+
+  if(req.body['Add Item']){
+    req.session.toDo.push({"name":req.body.name, "id":req.session.curId});
+    req.session.curId++;
+  }
+
+  if(req.body['Done']){
+    req.session.toDo = req.session.toDo.filter(function(e){
+      return e.id != req.body.id;
+    })
+  }
+
+  context.name = req.session.name;
+  context.toDoCount = req.session.toDo.length;
+  context.toDo = req.session.toDo;
+  console.log(context.toDo);
+  res.render('toDo',context);
+});
+
 app.use(function(req,res){
   res.type('text/plain');
   res.status(404);
