b47c042ecfba231f33b46c0c40771868926b01d2
mootrichard <richard@codemoot.com>
2016-03-08 21:43:01 -0800

    update for adding brewDB API wrapper

--- CS290/how-to-api/server.js
+++ CS290/how-to-api/server.js
@@ -2,6 +2,7 @@ var express = require('express'),
     app = express(),
     handlebars = require('express-handlebars'),
     bodyParser = require('body-parser'),
+    brewerydb = require('brewerydb-node');
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
