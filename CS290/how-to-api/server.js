e0cd3290e3ef079be448b5da5b9f2141187461c3
mootrichard <richard@codemoot.com>
2016-03-08 21:15:09 -0800

    update packages and server

---
+++ CS290/how-to-api/server.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+    app = express(),
+    handlebars = require('express-handlebars'),
+    bodyParser = require('body-parser'),
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
