8fcac62e71b2bfab71299272c27cce04529611da
mootrichard <richard@codemoot.com>
2016-03-08 22:23:11 -0800

    Adjusting extensions to support hbs for the view enginer

--- CS290/how-to-api/server.js
+++ CS290/how-to-api/server.js
@@ -4,9 +4,30 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     brewerydb = require('brewerydb-node');
 
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.engine('hbs', handlebars({defaultLayout: 'main', extname:'.hbs'}));
+app.set('view engine', 'hbs');
 app.set('port', 3000);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.get('/', function(req,res){
+  res.render('home');
+});
+
+app.use(function(req,res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(req, res) {
+  console.log('Express started on http://localhost:' + app.get('port') + '; Press Ctrl+C to terminate.')
+});
