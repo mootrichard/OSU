a985cf1e36e57721c06ab5c1efce8b92547f86a9
richard <richard@codemoot.com>
2016-05-07 22:01:03 -0700

    Add more to selection queries

--- CS340/assignment2/Assignment2-selection.sql
+++ CS340/assignment2/Assignment2-selection.sql
@@ -2,13 +2,44 @@
 #In other words let L be the maximum film length, and let R be the minimum rental duration in the table film. You need to find all films with length L and rental duration R.
 #You just need to return attribute film id for this query. 
 
+SELECT film_id 
+FROM film 
+WHERE length=(SELECT MAX(length) FROM film) 
+AND rental_duration=(SELECT MAX(rental_duration) FROM film);
 
 #2 We want to find out how many of each category of film ED CHASE has started in so return a table with category.name and the count
 #of the number of films that ED was in which were in that category order by the category name ascending (Your query should return every category even if ED has been in no films in that category).
 
+SELECT name, SUM(CASE WHEN actor.first_name="ED" 
+					AND actor.last_name="CHASE" 
+                    THEN 1 ELSE 0 END) as ed_is_in
+FROM category 
+INNER JOIN film_category
+	ON category.category_id=film_category.category_id
+INNER JOIN film
+	ON film.film_id=film_category.film_id
+INNER JOIN film_actor
+	ON film_actor.film_id=film.film_id
+INNER JOIN actor
+	ON actor.actor_id=film_actor.actor_id
+GROUP BY category.name
+ORDER BY ed_is_in DESC;
 
 #3 Find the first name, last name and total combined film length of Sci-Fi films for every actor
 #That is the result should list the names of all of the actors(even if an actor has not been in any Sci-Fi films)and the total length of Sci-Fi films they have been in.
+
+SELECT first_name, last_name, 
+SUM(CASE WHEN category.name="Sci-Fi" THEN film.length ELSE 0 END) as sci_fi_length
+FROM actor
+INNER JOIN film_actor
+	ON film_actor.actor_id=actor.actor_id
+INNER JOIN film
+	ON film.film_id=film_actor.film_id
+INNER JOIN film_category
+	ON film_category.film_id=film.film_id
+INNER JOIN category
+	ON category.category_id=film_category.category_id
+GROUP BY actor.first_name, actor.last_name
 
 
 #4 Find the first name and last name of all actors who have never been in a Sci-Fi film
