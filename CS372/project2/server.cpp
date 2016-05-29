0fbdfb0ad2cb0886cbd2ee54dbf79a7e68649165
richard <richard@codemoot.com>
2016-05-29 13:40:58 -0700

    Add description at beginning of file

--- CS372/project2/server.cpp
+++ CS372/project2/server.cpp
@@ -1,3 +1,19 @@
+/**
+ * Author: Richard Moot
+ * 		CS372 - Project 2 - FTP Server
+ * Date: May 29, 2016
+ * Description:
+ * 		This is the server component of the FTP Server/Client interation. The user
+ * 		must provide a port number for the server to run on as an argument. The server
+ * 		has several commadns that are received over the command port that allow the user
+ * 		to list the directory contents and retrieve files from the server.
+ *
+ * 		The server utilizes socket connections and will remain listening on the specified
+ * 		port for a connection to be made. Once the connection is established the client
+ * 		can then specify which port they would like the data to be received on and the server
+ * 		will deliver files over that port.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
