960f5e28045e17f2d9756255e983c5f026df7bc6
richard <richard@codemoot.com>
2016-05-29 18:57:07 -0700

    In functioning state

--- CS372/Program_Two/server.cpp
+++ CS372/Program_Two/server.cpp
@@ -31,7 +31,12 @@
 
 // Function Prototypes
 void signalHandler(int signum);
+void error (std::string msg);
+void startup(int commPort, int newSocket);
+char *handleRequest(int commSocket);
+int setupSocket(int commPort);
 
+// Main execution of FTP Server
 int main(int argc, char const *argv[]) {
 
   // Ensure that a port number has been specified, additionally, prompt with correct usage
@@ -40,20 +45,111 @@ int main(int argc, char const *argv[]) {
     exit(1);
   }
 
+  // Gracefully handling signal interrupts for shutting down the server
   signal(SIGINT, signalHandler);
+
+  // Initializing FTP Server
+  int commPort = atoi(argv[1]);
+  int serverSocket = setupSocket(commPort);
+
+  startup(commPort, serverSocket);
 
   return 0;
 }
 
-void startup (){
+/**
+ * [setupSocket description]
+ * @param  commPort [description]
+ * @return          [description]
+ */
+int setupSocket(int commPort){
+  int newSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if(newSocket < 0){
+    error("ERROR trying to open socket");
+  };
 
+  struct sockaddr_in serverAddr;
+
+  bzero((char *) &serverAddr, sizeof(serverAddr));
+
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_addr.s_addr = INADDR_ANY;
+  serverAddr.sin_port = htons(commPort);
+
+  if(bind(newSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr))< 0){
+    error("Error in binding to provided port");
+    exit(1);
+  };
+
+  listen(newSocket, 5);
+
+  return newSocket;
 };
 
-void handleRequest(){
+/**
+ * [startup  description]
+ * @param commPort  [description]
+ * @param newSocket [description]
+ */
+void startup (int commPort, int newSocket){
+  while(true){
+    int commSocket, dataSocket;
+    char* incComm;
 
+    struct sockaddr_in clientAddr; // Client's address stored here
+
+    socklen_t clientLength = sizeof(clientAddr); // Creating length for accept() of client's address
+
+    std::cout << "Server running and listening on port " << commPort << "..." << std::endl;
+
+    // Listening on the Command socket for any requests from client(s)
+    commSocket = accept(newSocket, (struct sockaddr *) &clientAddr, &clientLength);
+    if(commSocket < 0){ // Verifying that we successuflly accepted
+      error("Error: Command socket failed to initialize.\n");
+      exit(1);
+    }
+
+    incComm = handleRequest(commSocket);
+
+  }
 };
 
+char *handleRequest(int commSocket){
+  unsigned short packetLen;
+  unsigned short dataLen;
+  char* req;
+  int reqByteSize = 0;
+
+  int n = recv(commSocket, &packetLen, 2, 0);
+  packetLen = ntohs(packetLen);
+
+  dataLen = packetLen - sizeof(packetLen);
+
+  req = (char *)malloc(dataLen * sizeof(char));
+
+  while(reqByteSize < dataLen){
+    n = recv(commSocket, req + reqByteSize, (dataLen - reqByteSize),0);
+    reqByteSize += n;
+  }
+  req[dataLen] = '\0';
+
+  return req;
+};
+
+/**
+ * [signalHandler description]
+ * @param signum [description]
+ */
 void signalHandler(int signum){
   std::cout << "Server shutting down..." << std::endl;
   exit(signum);
+}
+
+/**
+ * [error  description]
+ * @param msg [description]
+ */
+void error (std::string msg) {
+  std::cerr << msg << std::endl;
+  exit(1);
 }
