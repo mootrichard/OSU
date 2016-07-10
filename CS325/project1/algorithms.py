20815c75e3ddbc89c4b31205f0b934008cf03bb9
richard <richard@codemoot.com>
2016-07-10 12:33:01 -0700

    Implement enumeration algorith

--- CS325/project1/algorithms.py
+++ CS325/project1/algorithms.py
@@ -1,3 +1,21 @@
+from datetime import datetime
+
+def enumeration_algo(arr):
+    start = 0
+    end = len(arr)
+    sub_sum = arr[0]
+
+    for i in xrange(len(arr)):
+        for j in xrange(i, len(arr)):
+            temp_sum = 0
+            for k in xrange(i, j + 1):
+                temp_sum += arr[k]
+                if sub_sum < temp_sum:
+                    start = i
+                    end = j
+                    sub_sum = max(sub_sum, temp_sum)
+
+    return (start, end, sub_sum)
 
 with open('MSS_TestProblems.txt', 'r') as test_problems:
     for line in test_problems:
@@ -7,4 +25,4 @@ with open('MSS_TestProblems.txt', 'r') as test_problems:
         output_array = []
         for i in input_array:
             output_array.append(int(i))
-        print output_array
+        print enumeration_algo(output_array)
