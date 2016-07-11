3ba60772b6f8fd21258284225cfa6f814351e13a
richard <richard@codemoot.com>
2016-07-11 15:48:50 -0700

    Fix divide_and_conquer algorithm to work correctly for all input values

--- CS325/project1/algorithms.py
+++ CS325/project1/algorithms.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 from sys import maxint
+from math import floor, ceil
+
+test_arrays = list()
 
 def enumeration_algo(arr):
     start = 0
@@ -13,10 +16,10 @@ def enumeration_algo(arr):
                 temp_sum += arr[k]
                 if sub_sum < temp_sum:
                     start = i
-                    end = j
+                    end = j+1
                     sub_sum = max(sub_sum, temp_sum)
 
-    return (start, end, sub_sum)
+    return arr[start:end], sub_sum
 
 def enumeration_algo_faster(arr):
     start = 0
@@ -29,12 +32,61 @@ def enumeration_algo_faster(arr):
             temp_sum += arr[j]
             if sub_sum < temp_sum:
                 start = i
-                end = j
+                end = j+1
                 sub_sum = max(sub_sum, temp_sum)
-    return (start, end, sub_sum)
+    return arr[start:end], sub_sum
 
-def test(inc_function):
+def divide_and_conquer(arr, start=None, end=None):
+    if start == None:
+        start = 0
+    if end == None:
+        end = len(arr) - 1
+    def cross_over(arr, start, end, middle):
+        right_sum = -maxint - 1
+        left_sum = -maxint - 1
+        left_index_end = 0
+        right_index_end = 0
+
+        curr_left_sum = 0
+        for i in range(middle, start-1, -1):
+            curr_left_sum += arr[i]
+            if curr_left_sum > left_sum:
+                left_sum = curr_left_sum
+                left_index_end = i
+
+        curr_right_sum = 0
+        for j in range(middle+1, len(arr)):
+            curr_right_sum += arr[j]
+            if curr_right_sum > right_sum:
+                right_sum = curr_right_sum
+                right_index_end = j+1
+
+        return arr[left_index_end:right_index_end], left_sum + right_sum
+
+    if start == end:
+        return arr[start:end], arr[start]
+
+    middle = (start + end) / 2
+
+    left_sub, left_sum = divide_and_conquer(arr, start, middle)
+    right_sub, right_sum = divide_and_conquer(arr, middle+1, end)
+    cross_sub, cross_sum = cross_over(arr, start, end , middle)
+
+    if left_sum > right_sum:
+        if left_sum > cross_sum:
+            return (left_sub, left_sum)
+        else:
+            return (cross_sub, cross_sum)
+    else:
+        if right_sum > cross_sum:
+            return (right_sub, right_sum)
+        else:
+            return (cross_sub, cross_sum)
+
+
+def extract():
     with open('MSS_TestProblems.txt', 'r') as test_problems:
+        list_of_arrays = list()
         for line in test_problems:
             line = line.replace("[", "")
             line = line.replace("]", "")
@@ -42,9 +94,20 @@ def test(inc_function):
             output_array = []
             for i in input_array:
                 output_array.append(int(i))
-            print inc_function(output_array)
+            list_of_arrays.append(output_array)
+    return list_of_arrays
 
-print "n^3"
-test(enumeration_algo)
-print "n^2"
-test(enumeration_algo_faster)
+list_arrays = extract()
+
+print ""
+print "O(n^3)"
+for i in list_arrays:
+    print enumeration_algo(i)
+print ""
+print "O(n^2)"
+for i in list_arrays:
+    print enumeration_algo_faster(i)
+print ""
+print "O(n log n)"
+for i in list_arrays:
+    print divide_and_conquer(i, 0, len(i) - 1)
